0409c6c27e1831c267354cd9a7099194302343e7
advmczvk <kuba.adamczyk99@gmail.com>
2021-10-19 19:56:22 +0200

    #8 edit hero

--- frontend/src/containers/home/Home.tsx
+++ frontend/src/containers/home/Home.tsx
@@ -6,10 +6,10 @@ export default function Home() {
         <div className="content-section">
             <div className="container hero-container">
                 <div className="img-container">
-                    {/* <img id="heroImg" src="http://localhost:3000/hero.jpeg" alt="hero"/> */}
+                    <img id="heroImg" src="http://localhost:3000/hero.jpeg" alt="hero"/>
                 </div>
                 <div className="hero-block-info">
-                    <h2>New tournament</h2>
+                    <h2>About us</h2>
                     <h3>Take part in a new tournament, join now, create team and invite your friends.</h3>
                     <br/>
                     <p>Together more fun.</p>

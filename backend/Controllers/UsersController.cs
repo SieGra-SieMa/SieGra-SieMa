8ab0d83208930ce630875398bf23cd3d57a66627
Janek9709 <jan.biniek97@gmail.com>
2022-05-21 08:49:01 +0200

    #83 user crud

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SieGraSieMa.DTOs.ErrorDTO;
 using SieGraSieMa.DTOs.Users;
 using SieGraSieMa.Models;
 using SieGraSieMa.Services.Interfaces;
@@ -55,6 +56,33 @@ namespace SieGraSieMa.Controllers
             var roles = await _userManager.GetRolesAsync(user);
             return Ok(new UserDTO { Id = user.Id, Name = user.Name, Surname = user.Surname, Email = user.NormalizedEmail, Roles = roles });
             //new UserDTO { Id = user.Id, Name=user.Name, Surname=user.Surname, Email = user.NormalizedEmail}
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetUserById(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound(new ResponseErrorDTO { Error = "User not found" });
+            var roles = await _userManager.GetRolesAsync(user);
+            return Ok(new UserDTO { Id = user.Id, Name = user.Name, Surname = user.Surname, Email = user.NormalizedEmail, Roles = roles });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound(new ResponseErrorDTO { Error = "User not found" });
+            await _userManager.DeleteAsync(user);
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult GetUsers()
+        {
+            var users = _userManager.Users.ToList();
+            return Ok(users.Select(u => new UserDTO { Id = u.Id, Name = u.Name, Surname = u.Surname, Email = u.NormalizedEmail }));
         }
     }
 }

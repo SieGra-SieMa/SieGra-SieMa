8c75be587edeb6b685b7fea1e81823b678691c20
Kuba Adamczyk <kuba.adamczyk99@gmail.com>
2022-05-27 00:00:12 +0200

    feat(aboutUs): :sparkles: add about us section

--- frontend/src/components/about-us/AboutUs.tsx
+++ frontend/src/components/about-us/AboutUs.tsx
@@ -1,5 +1,13 @@
 export default function AboutUs() {
     return(
-        <div>About us</div>
+        <div className="abour-us-section">
+            <div className="description">
+                <h2>O nas</h2>
+                <p>
+                    Cześć, miło, że nas odwiedziłeś! Jesteśmy paczką przyjaciół i wspólnie dzielimy pasje do koszykówki i pomagania. 
+                    Od 
+                </p>
+            </div>
+        </div>
     )
 }

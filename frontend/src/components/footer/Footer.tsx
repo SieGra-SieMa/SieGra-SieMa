44ddb5377ca4affa3ae03a47f95d2dd87dca7837
advmczvk <kuba.adamczyk99@gmail.com>
2022-06-07 11:09:22 +0200

    fix(footer): :lipstick: make footer css a module and add mobile styles

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,27 +1,27 @@
 import { Link } from 'react-router-dom';
-import './Footer.css';
+import styles from './Footer.module.css';
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
 
 export default function Footer() {
     return (
-        <div className="footer-section">
-            <div className="container">
-                <div className="footer-container">
-                    <div className="footer-block">
+        <div className={styles.footerSection}>
+            <div className={styles.container}>
+                <div className={styles.footerContainer}>
+                    <div className={styles.footerBlock}>
                         <Link to="/">
-                            <img className="footer-icon" src="/logo_w.png" alt="" />
+                            <img className={styles.footerIcon} src="/logo_w.png" alt="" />
                         </Link>
                     </div>
-                    <div className="footer-block">
+                    <div className={styles.footerBlock}>
                         <a className="slide-button" href="mailto:siegrasiema.inicjatywa@gmail.com"><p>Get in touch</p></a>
                     </div>
-                    <div className="footer-block">
-                        <div className='icon-text'><PhoneIcon/> <p>796 688 795</p></div>
-                        <div className='icon-text'><EmailIcon/> <p>siegrasiema.inicjatywa@gmail.com</p></div>
+                    <div className={styles.footerBlock}>
+                        <div className={styles.iconText}><PhoneIcon/> <p>796 688 795</p></div>
+                        <div className={styles.iconText}><EmailIcon/> <p>siegrasiema.inicjatywa@gmail.com</p></div>
                     </div>
                 </div>
-                <p id='designed-by'>
+                <p id={styles.designedBy}>
                     Designed & created by Jakub Adamczyk, Jan Biniek, Taras Kulyavets, Jakub Pawłowicz
                 </p>
             </div>

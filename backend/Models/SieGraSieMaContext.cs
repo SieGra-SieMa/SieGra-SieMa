3866adb2880905532a025f51caf7c74da1bc0752
kubbit98 <kuba98pawlowicz@gmail.com>
2022-06-10 19:55:13 +0200

    prepare to deployment

--- backend/Models/SieGraSieMaContext.cs
+++ backend/Models/SieGraSieMaContext.cs
@@ -46,7 +46,7 @@ namespace SieGraSieMa.Models
                 //You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration
                 //- see https://go.microsoft.com/fwlink/?linkid=2131148.
                 //For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySQL("Server=localhost;database=SieGraSieMa;user=siegra;password=siema");
+                //optionsBuilder.UseMySQL("Server=localhost;database=SieGraSieMa;user=siegra;password=siema");
 
             }
         }

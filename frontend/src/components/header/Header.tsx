4bc2110e72d45c00783542ae561df8ea7754f531
Kuba Adamczyk <kuba.adamczyk99@gmail.com>
2022-06-12 00:16:16 +0200

    feat(header): add active link underline

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,73 +1,76 @@
-import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
-import { useAuth } from '../auth/AuthContext';
-import { useUser } from '../user/UserContext';
-import { useCallback, useState } from 'react';
-import Button from '../form/Button';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
+import styles from "./Header.module.css";
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../auth/AuthContext";
+import { useUser } from "../user/UserContext";
+import { useCallback, useState } from "react";
+import Button from "../form/Button";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Header() {
+	const { session, setSession } = useAuth();
+	const { user } = useUser();
+	const [toggleMenu, setToggleMenu] = useState(false);
+	const [navState, setNavState] = useState(`${styles.navClosed}`);
 
-    const { session, setSession } = useAuth();
-    const { user } = useUser();
-    const [toggleMenu, setToggleMenu] = useState(false);
-    const [navState, setNavState] = useState(`${styles.navClosed}`);
+	const logout = useCallback(() => setSession(null), [setSession]);
 
-    const logout = useCallback(() => setSession(null), [setSession]);
+	const toggleNav = () => {
+		setToggleMenu(!toggleMenu);
+		setNavState(!toggleMenu ? `${styles.navOpen}` : `${styles.navClosed}`);
+	};
 
-    const toggleNav = () => {
-        setToggleMenu(!toggleMenu);
-        setNavState( !toggleMenu ? `${styles.navOpen}` : `${styles.navClosed}`);
-    };
-
-    return (
-        <header className={styles.root}>
-            <div className={styles.container}>
-                <div className={styles.logo}>
-                    <Link to="/">
-                        <img src="/logo_w.png" alt="" />
-                    </Link>
-                </div>
-                <nav className={styles.navigation} id={ navState }>
-                    <ul>
-                        <li>
-                            <Link to="/">
-                                Strona główna
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/tournaments">
-                                Turnieje
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/about-us">
-                                O nas
-                            </Link>
-                        </li>
-                        {session ? (
-                            <>
-                                <li>
-                                    <Link to='/account'>
-                                        {user ? `${user.name} ${user.surname}` : 'USERNAME'}
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Button value='Wyloguj' type='button' onClick={logout} />
-                                </li>
-                            </>
-                        ) : (
-                            <li>
-                                <Link to="/account">
-                                    Profil
-                                </Link>
-                            </li>
-                        )}
-                    </ul>
-                </nav>
-            </div>
-            <button className={styles.menu} onClick={toggleNav}>{ !toggleMenu ? <MenuIcon fontSize="large"/> : <CloseIcon fontSize="large"/> }</button>
-        </header >
-    );
+	return (
+		<header className={styles.root}>
+			<div className={styles.container}>
+				<div className={styles.logo}>
+					<NavLink to="/">
+						<img src="/logo_w.png" alt="" />
+					</NavLink>
+				</div>
+				<nav className={styles.navigation} id={navState}>
+					<ul>
+						<li>
+							<NavLink className="navlink" to="/">Strona główna</NavLink>
+						</li>
+						<li>
+							<NavLink className="navlink" to="/tournaments">Turnieje</NavLink>
+						</li>
+						<li>
+							<NavLink className="navlink" to="/about-us">O nas</NavLink>
+						</li>
+						{session ? (
+							<>
+								<li>
+									<NavLink className="navlink" to="/account">
+										{user
+											? `${user.name} ${user.surname}`
+											: "USERNAME"}
+									</NavLink>
+								</li>
+								<li>
+									<Button
+										value="Wyloguj"
+										type="button"
+										onClick={logout}
+									/>
+								</li>
+							</>
+						) : (
+							<li>
+								<NavLink className="navlink" to="/account">Profil</NavLink>
+							</li>
+						)}
+					</ul>
+				</nav>
+			</div>
+			<button className={styles.menu} onClick={toggleNav}>
+				{!toggleMenu ? (
+					<MenuIcon fontSize="large" />
+				) : (
+					<CloseIcon fontSize="large" />
+				)}
+			</button>
+		</header>
+	);
 }

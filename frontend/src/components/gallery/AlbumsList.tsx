c273788f4536bd08da71bc22e7619568276ff300
Kuba Adamczyk <kuba.adamczyk99@gmail.com>
2022-06-26 01:05:14 +0200

    feat: add albums icons

--- frontend/src/components/gallery/AlbumsList.tsx
+++ frontend/src/components/gallery/AlbumsList.tsx
@@ -1,92 +1,108 @@
-import Config from '../../config.json';
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { ROLES } from '../../_lib/roles';
-import { useApi } from '../api/ApiContext';
-import styles from './AlbumsList.module.css';
-import Button from '../form/Button';
-import Modal from '../modal/Modal';
-import CreateAlbum from './CreateAlbum';
-import GuardComponent from '../guard-components/GuardComponent';
-import { TournamentWithAlbums } from '../../_lib/_types/tournament';
-import ImageIcon from '@mui/icons-material/Image';
+import Config from "../../config.json";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ROLES } from "../../_lib/roles";
+import { useApi } from "../api/ApiContext";
+import styles from "./AlbumsList.module.css";
+import Button from "../form/Button";
+import Modal from "../modal/Modal";
+import CreateAlbum from "./CreateAlbum";
+import GuardComponent from "../guard-components/GuardComponent";
+import { TournamentWithAlbums } from "../../_lib/_types/tournament";
+import ImageIcon from "@mui/icons-material/Image";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function AlbumsList() {
+	const navigate = useNavigate();
 
-    const navigate = useNavigate();
+	const { tournamentsService } = useApi();
 
-    const { tournamentsService } = useApi();
+	const { id } = useParams<{ id: string }>();
 
-    const { id } = useParams<{ id: string }>();
+	const [tournamentWithAlbums, setTournamentWithAlbums] =
+		useState<TournamentWithAlbums | null>(null);
+	const [isAddAlbum, setIsAddAlbum] = useState(false);
 
-    const [tournamentWithAlbums, setTournamentWithAlbums] = useState<TournamentWithAlbums | null>(null);
-    const [isAddAlbum, setIsAddAlbum] = useState(false);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+	useEffect(() => {
+		tournamentsService.getTournamentWithAlbums(id!).then((data) => {
+			setTournamentWithAlbums(data);
+		});
+	}, [id, tournamentsService]);
 
-    useEffect(() => {
-        tournamentsService.getTournamentWithAlbums(id!)
-            .then((data) => {
-                setTournamentWithAlbums(data);
-            });
-    }, [id, tournamentsService])
+	return (
+		<>
+			<div className={styles.top}>
+				<ArrowBackIosNewIcon
+					className="interactiveIcon"
+					onClick={() => navigate("../..")}
+					fontSize="large"
+				/>
+				<GuardComponent roles={[ROLES.Admin]}>
+					<AddIcon
+						className="interactiveIcon"
+						onClick={() => setIsAddAlbum(true)}
+						fontSize="large"
+					/>
+				</GuardComponent>
+			</div>
 
-    return (
-        <>
-            <div className={styles.top}>
-                <Button value='Wstecz' onClick={() => navigate('../..')} />
-                <GuardComponent roles={[ROLES.Admin]}>
-                    <Button
-                        value='Dodaj album'
-                        onClick={() => setIsAddAlbum(true)}
-                    />
-                </GuardComponent>
-            </div>
+			{tournamentWithAlbums && (
+				<>
+					<h1 className={styles.title}>
+						{tournamentWithAlbums.name}
+					</h1>
+					<h4>Albumy</h4>
+					<ul className={styles.albums}>
+						{tournamentWithAlbums.albums.map((album, index) => (
+							<li
+								key={index}
+								className={styles.item}
+								style={
+									album.profilePicture
+										? {
+												backgroundImage: `url(${Config.HOST}${album.profilePicture})`,
+										  }
+										: undefined
+								}
+								onClick={() => navigate(`${album.id}`)}
+							>
+								<div className={styles.box}>
+									{!album.profilePicture && (
+										<ImageIcon className={styles.picture} />
+									)}
+									<h4>{album.name}</h4>
+								</div>
+							</li>
+						))}
+					</ul>
+				</>
+			)}
 
-            {tournamentWithAlbums && (<>
-                <h6 className={styles.breadcrumbs}>Galeria</h6>
-                <h1 className={styles.title}>{tournamentWithAlbums.name}</h1>
-                <h4>Albumy</h4>
-                <ul className={styles.albums}>
-                    {tournamentWithAlbums.albums.map((album, index) => (
-                        <li
-                            key={index}
-                            className={styles.item}
-                            style={album.profilePicture ? {
-                                backgroundImage: `url(${Config.HOST}${album.profilePicture})`,
-                            } : undefined}
-                            onClick={() => navigate(`${album.id}`)}>
-                            <div className={styles.box}>
-                                {(!album.profilePicture) && <ImageIcon className={styles.picture} />}
-                                <h4>
-                                    {album.name}
-                                </h4>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </>)}
-
-            {(tournamentWithAlbums && isAddAlbum) && (
-                <Modal
-                    title='Dodaj album'
-                    close={() => setIsAddAlbum(false)}
-                    isClose
-                >
-                    <CreateAlbum
-                        tournamentId={id!}
-                        confirm={(album) => {
-                            setTournamentWithAlbums({
-                                ...tournamentWithAlbums,
-                                albums: tournamentWithAlbums.albums.concat(album)
-                            });
-                            setIsAddAlbum(false);
-                        }}
-                    />
-                </Modal>
-            )}
-        </>
-    );
+			{tournamentWithAlbums && isAddAlbum && (
+				<Modal
+					title="Dodaj album"
+					close={() => setIsAddAlbum(false)}
+					isClose
+				>
+					<CreateAlbum
+						tournamentId={id!}
+						confirm={(album) => {
+							setTournamentWithAlbums({
+								...tournamentWithAlbums,
+								albums: tournamentWithAlbums.albums.concat(
+									album
+								),
+							});
+							setIsAddAlbum(false);
+						}}
+					/>
+				</Modal>
+			)}
+		</>
+	);
 }

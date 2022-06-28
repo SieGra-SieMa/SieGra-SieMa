a67bc07f4b4be9a2a29503d9a1de6b02cad41b19
Janek9709 <jan.biniek97@gmail.com>
2022-06-28 03:08:41 +0200

    #203 conflict resolved

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -16,7 +16,6 @@ import AccountEntry from '../account-entry/AccountEntry';
 import TeamsList from '../teams/TeamsList';
 import Tournaments from '../tournaments/Tournaments';
 import { ROLES } from '../../_lib/roles';
-import { ApiContext } from '../api/ApiContext';
 import ApiClient from '../../_services';
 import UserProvider from '../user/UserProvider';
 import TournamentsList from '../tournaments/list/TournamentsList';
@@ -27,64 +26,67 @@ import AccountData from '../profile/Profile';
 import AdminPanel from '../admin/AdminPanel';
 import AdminTeamsList from '../admin/teams/AdminTeamsList';
 import Newsletter from '../admin/Newsletter';
-import ConfirmEmail from '../account-entry/ConfirmEmail';
-import ResetPassword from '../account-entry/ResetPassword'
+import AlertProvider from '../alert/AlertProvider';
+import ApiProvider from '../api/ApiProvider';
 
 const apiClient = new ApiClient();
 
 export default function App() {
     return (
-        <ApiContext.Provider value={apiClient}>
-            <AuthProvider>
-                <UserProvider>
-                    <BrowserRouter>
-                        <Header />
-                        <Routes>
-                            <Route index element={<Home />} />
-                            <Route path="about-us" element={<AboutUs />} />
-                            <Route path="entry" element={<AccountEntry />} />
-                            <Route path="access-denied" element={
-                                <div style={{ color: '#fff' }}>ACCESS DENIED</div>
-                            } />
-                            <Route path="account" element={
-                                <GuardRoute roles={[ROLES.User, ROLES.Employee, ROLES.Admin]} />
-                            }>
-                                <Route index element={<AccountData />} />
-                            </Route>
-                            <Route path="myteams" element={
-                                <GuardRoute roles={[ROLES.User, ROLES.Employee, ROLES.Admin]} />
-                            }>
-                                <Route index element={<TeamsList />} />
-                            </Route>
-                            <Route path="admin" element={
-                                <GuardRoute roles={[ROLES.Admin]} />
-                            }>
-                                <Route path='' element={<AdminPanel />} >
-                                    <Route index element={<Newsletter />} />
-                                    <Route path="users" element={<UsersList />} />
-                                    <Route path="teams" element={<AdminTeamsList />} />
+        <AlertProvider options={{
+            errorTimeout: 3000,
+            successTimeout: 7000
+        }}>
+            <ApiProvider value={apiClient}>
+                <AuthProvider>
+                    <UserProvider>
+                        <BrowserRouter>
+                            <Header />
+                            <Routes>
+                                <Route index element={<Home />} />
+                                <Route path="about-us" element={<AboutUs />} />
+                                <Route path="entry" element={<AccountEntry />} />
+                                <Route path="access-denied" element={
+                                    <div style={{ color: '#fff' }}>ACCESS DENIED</div>
+                                } />
+                                <Route path="account" element={
+                                    <GuardRoute roles={[ROLES.User, ROLES.Employee, ROLES.Admin]} />
+                                }>
+                                    <Route index element={<AccountData />} />
                                 </Route>
-                            </Route>
-                            <Route path="*" element={<Tournaments />}>
-                                <Route path="tournaments">
-                                    <Route index element={<TournamentsList />} />
-                                    <Route path=":id" element={<Tournament />} />
+                                <Route path="myteams" element={
+                                    <GuardRoute roles={[ROLES.User, ROLES.Employee, ROLES.Admin]} />
+                                }>
+                                    <Route index element={<TeamsList />} />
                                 </Route>
-                                <Route path="gallery">
-                                    <Route index element={<GalleryList />} />
-                                    <Route path=":id/albums">
-                                        <Route index element={<AlbumsList />} />
-                                        <Route path=":albumId" element={<Album />} />
+                                <Route path="admin" element={
+                                    <GuardRoute roles={[ROLES.Admin]} />
+                                }>
+                                    <Route path='' element={<AdminPanel />} >
+                                        <Route index element={<Newsletter />} />
+                                        <Route path="users" element={<UsersList />} />
+                                        <Route path="teams" element={<AdminTeamsList />} />
                                     </Route>
                                 </Route>
-                            </Route>
-                            <Route path='email-confirmation' element={<ConfirmEmail />} />
-                            <Route path='reset-password' element={<ResetPassword />} />
-                        </Routes >
-                        <Footer />
-                    </BrowserRouter >
-                </UserProvider >
-            </AuthProvider >
-        </ApiContext.Provider >
+                                <Route path="*" element={<Tournaments />}>
+                                    <Route path="tournaments">
+                                        <Route index element={<TournamentsList />} />
+                                        <Route path=":id" element={<Tournament />} />
+                                    </Route>
+                                    <Route path="gallery">
+                                        <Route index element={<GalleryList />} />
+                                        <Route path=":id/albums">
+                                            <Route index element={<AlbumsList />} />
+                                            <Route path=":albumId" element={<Album />} />
+                                        </Route>
+                                    </Route>
+                                </Route>
+                            </Routes >
+                            <Footer />
+                        </BrowserRouter >
+                    </UserProvider >
+                </AuthProvider >
+            </ApiProvider>
+        </AlertProvider>
     );
 };

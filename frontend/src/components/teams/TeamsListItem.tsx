f5478cd0b86af7a88cbc2760b023d52e5721d55c
Taras Kulyavets <taras1808@gmail.com>
2022-07-10 20:55:47 +0200

    fix(team-picture): bug fix

--- frontend/src/components/teams/TeamsListItem.tsx
+++ frontend/src/components/teams/TeamsListItem.tsx
@@ -174,7 +174,6 @@ export default function TeamsListItem({
                                 setTeams(data);
                             }
                         }}
-                        isAdmin
                     />
                 </Modal>
             )}

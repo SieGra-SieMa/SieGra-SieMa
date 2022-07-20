389e7991892fb963a78df4a847c77b957cc26826
Taras Kulyavets <taras1808@gmail.com>
2022-07-20 09:40:20 +0200

    feat: user search added to contests

--- frontend/src/components/tournaments/contests/AddScoreContest.tsx
+++ frontend/src/components/tournaments/contests/AddScoreContest.tsx
@@ -1,15 +1,28 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
+import Select from 'react-select'
+import { User } from '../../../_lib/types';
 import { Contest, Contestant } from '../../../_lib/_types/tournament';
 import { useApi } from '../../api/ApiContext';
 import Button from '../../form/Button';
 import Form from '../../form/Form';
 import Input from '../../form/Input';
 import VerticalSpacing from '../../spacing/VerticalSpacing';
-
+import styles from './Contests.module.css';
 
 type Props = {
     contest: Contest;
     confirm: (data: Contestant) => void;
+}
+
+const customStyles = {
+    control: (provided: any) => ({
+        ...provided,
+        padding: "5px 0"
+    }),
+    menuList: (provided: any) => ({
+        ...provided,
+        maxHeight: "175px",
+    }),
 }
 
 export default function AddScoreContest({
@@ -17,14 +30,33 @@ export default function AddScoreContest({
     confirm,
 }: Props) {
 
-    const { tournamentsService } = useApi();
+    const { tournamentsService, usersService } = useApi();
 
-    const [email, setEmail] = useState('');
+    const [search, setSearch] = useState('');
     const [points, setPoints] = useState(0);
+
+    const [options, setOptions] = useState<{ label: string, value: User }[]>();
+
+    const [selectedOption, setSelectedOption] = useState<{ label: string, value: User } | null>();
+
+    useEffect(() => {
+        return usersService.getUsers(1, 10, search)
+            .then((data) => setOptions(data.items.map((item) => ({
+                label: item.email,
+                value: item,
+            }))))
+            .abort;
+    }, [search, usersService])
 
     const onSubmit = (e: FormEvent) => {
         e.preventDefault();
-        return tournamentsService.addContestScore(contest.tournamentId, contest.id, email, points)
+        if (!selectedOption) return;
+        return tournamentsService.addContestScore(
+            contest.tournamentId,
+            contest.id,
+            selectedOption.value.email,
+            points
+        )
             .then((data) => {
                 confirm(data);
             });
@@ -35,13 +67,26 @@ export default function AddScoreContest({
             <VerticalSpacing size={15} />
             <Button value='Dodaj wynik' />
         </>}>
-            <Input
-                id='AddScoreContest-name'
-                label='Email'
-                value={email}
-                required
-                onChange={(e) => setEmail(e.target.value)}
-            />
+            <div>
+                <label htmlFor="test">Użytkownik</label>
+                <VerticalSpacing size={5} />
+                <Select
+                    styles={customStyles}
+                    placeholder="Wyszukaj..."
+                    options={options}
+                    onInputChange={(e) => setSearch(e)}
+                    onChange={(e) => setSelectedOption(e)}
+                    value={selectedOption}
+                    formatOptionLabel={({ value, label }) => (
+                        <div className={styles.option}>
+                            <div className={styles.optionTitle}>
+                                {value.name} {value.surname}
+                            </div>
+                            <div>{label}</div>
+                        </div>
+                    )}
+                />
+            </div>
             <Input
                 id='AddScoreContest-points'
                 label='Punkty'
